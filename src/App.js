2dead4da7f0f0ce91b0dbbb5f71e6056cb6a0ede
nightfury214 <207950995+nightfury214@users.noreply.github.com>
2025-04-20 20:06:27 -0700

    add Squre  and Board function

---
+++ src/App.js
@@ -0,0 +1,66 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React, { useState } from 'react';
+
+
+function Square() {
+  const [value, setValue] = useState(null);
+  function handleClick() {
+    setValue('X');
+  }
+  return (
+    <button 
+      className="square"
+      onClick={handleClick}  
+    >
+      {value}
+    </button>
+  );
+}
+
+export default function Board(){
+  return (
+    <>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+    </>
+  );
+}
+
